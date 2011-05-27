6c07c7890d114b955b780337722d8909117d40e1
Angad <angad@angad.sg>
2011-05-27 23:14:48 +0800

    added tcp_socket. on screen debug messages

--- src/org/umit/android/javasockets/javasockets.java
+++ src/org/umit/android/javasockets/javasockets.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
@@ -12,21 +13,33 @@ import java.util.Enumeration;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class javasockets extends Activity {
     
 	//yahoo's ip address
 	public static String serverip = "67.195.160.76";
+	TextView t;
+	EditText ip;
    	Process p;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ip = (EditText)findViewById(R.id.ip);
+        Editable host = ip.getText();
+        serverip = host.toString();
+        
+        t = (TextView)findViewById(R.id.msg);
+        t.setText("");
         
         //isReachable
         Button reachable = (Button)findViewById(R.id.ping_reachable);
@@ -49,18 +62,18 @@ public class javasockets extends Activity {
         pingshell.setOnClickListener(command_ping);
                
         //C code ping
-        Button pingnative = (Button)findViewById(R.id.native_ping);
-        pingnative.setOnClickListener(native_ping);
-        
+        Button socket_tcp = (Button)findViewById(R.id.tcp_socket);
+        socket_tcp.setOnClickListener(tcp_socket);
     }
     
     //isReachable
     private void checkReachable(String address)
     {
-    	try {	
+    	try {
+    		showResult("Pinging " + address, "");
         	InetAddress addr = InetAddress.getByName(address);
         	boolean reachable = addr.isReachable(2000);
-        	Log.e("JAVASOCKET TEST isReachable", "Result = " + reachable);
+        	showResult("isReachable", " " + reachable);
         }
         catch (Exception e)
         {
@@ -71,6 +84,7 @@ public class javasockets extends Activity {
     //Socket Ping - Port 13
     private void ping_socket(String addr)
     {
+    	showResult("Pinging " + addr, "");
     	InetSocketAddress address;
     	SocketChannel channel = null;
     	try {
@@ -82,8 +96,9 @@ public class javasockets extends Activity {
 				if(connected) {
 					channel.close();
 					Log.e("JAVASOCKET TEST socket_ping", "connected");	
+					showResult("socket_ping", "false");
 				}
-				else Log.e("JAVASOCKET TEST socket_ping", "not connected");
+				else showResult("socket_ping", "not connected");
 				
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -98,6 +113,7 @@ public class javasockets extends Activity {
     private void ping_echo(String address)
     {
     	try {
+    		showResult("Pinging " + address, "");
     		ByteBuffer msg = ByteBuffer.wrap("Hello".getBytes());
     		ByteBuffer response = ByteBuffer.allocate("Hello".getBytes().length);
     		
@@ -110,7 +126,7 @@ public class javasockets extends Activity {
     		dgc.receive(response);
     		
     		String received = new String(response.array());
-			Log.e("JAVASOCKET TEST echo_ping", received);
+			showResult("echo_ping", received);
     		}
     	catch (Exception e)
     	{
@@ -121,20 +137,42 @@ public class javasockets extends Activity {
     //ping using the shell command
     private void ping_shell(String address)
     {
+    	showResult("Pinging " + address, "");
     	ping_thread t = new ping_thread(address);
     	t.run();
+    }
+    
+    private void socket_tcp(String address)
+    {
+    	showResult("TCP Socket to ", address);
+    	try{
+    		Socket s = new Socket(address, 80);
+    		if(s.isConnected())
+    		{
+    			showResult("socket_tcp", "connected");
+    		}
+    		else showResult("socket_tcp", "not connected");
+    	}
+    	
+    	catch(Exception e){
+    		e.printStackTrace();
+    	}
     }
 
     
     //---------onClick Event Handlers-----------//
     private OnClickListener ping_reachable = new OnClickListener() {
         public void onClick(View v) {
+            Editable host = ip.getText();
+            serverip = host.toString();
         	checkReachable(serverip);
         }
     };
         
     private OnClickListener socket_ping = new OnClickListener() {
         public void onClick(View v) {
+            Editable host = ip.getText();
+            serverip = host.toString();
         	ping_socket(serverip);
         }
     };
@@ -142,18 +180,23 @@ public class javasockets extends Activity {
     private OnClickListener network_interfaces = new OnClickListener() {
         public void onClick(View v) {
         	try {
+                Editable host = ip.getText();
+                serverip = host.toString();
+
         		for(Enumeration<NetworkInterface> list = NetworkInterface.getNetworkInterfaces(); list.hasMoreElements();){
         			NetworkInterface i = list.nextElement();
-        			Log.e("JAVASOCKET TEST network_interfaces", "display name " + i.getDisplayName());        			
+        			//Log.e("JAVASOCKET TEST network_interfaces", "display name " + i.getDisplayName());
+        			showResult("network_interfaces", "display name " + i.getDisplayName());
         			for(Enumeration<InetAddress> addresses = i.getInetAddresses(); addresses.hasMoreElements();)
         			{
         				String address = addresses.nextElement().toString().substring(1);
-        				Log.e("JAVASOCKET TEST InetAddress", address);
+        				//Log.e("JAVASOCKET TEST InetAddress", address);
+        				showResult("InetAddress", address);
         				
-        				ping_echo(address);
+        				//ping_echo(address);
         				checkReachable(address);
-        				ping_socket(address);
-        				ping_shell(address);
+        				//ping_socket(address);
+        				//ping_shell(address);
         			}
         		}
         	}
@@ -166,18 +209,46 @@ public class javasockets extends Activity {
     
     private OnClickListener echo_ping = new OnClickListener() {
         public void onClick(View v) {
+            Editable host = ip.getText();
+            serverip = host.toString();
+
         	ping_echo(serverip);
         }
     };
     
     private OnClickListener command_ping = new OnClickListener() {
         public void onClick(View v) {
+            Editable host = ip.getText();
+            serverip = host.toString();
+
         	ping_shell(serverip);
         }
     };
         
-    private OnClickListener native_ping = new OnClickListener() {
+    private OnClickListener tcp_socket = new OnClickListener() {
         public void onClick(View v) {
+            Editable host = ip.getText();
+            serverip = host.toString();
+            socket_tcp(serverip);
         }
-    };   
+    };
+    
+    private static int line_count = 0;
+    private static boolean isFull = false;
+    private void showResult(String method, String msg)
+    {
+    	if(line_count == 5 || isFull)
+    	{
+    		String txt = t.getText().toString();
+    		txt = txt.substring(txt.indexOf('\n') + 1);
+    		t.setText(txt);
+    		isFull=true;
+    		line_count = 0;
+    	}
+    	line_count++;
+    	
+    	Log.e("JAVASOCKET TEST", method + " " + msg);
+    	t.append(method + ": " + msg + "\n");
+    }
+    
 }//class
